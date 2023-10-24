784ab9b7e760493453d48f5abb2b2a93f18ee2b3
richardsnj1 <richard.senjaya1@gmail.com>
2023-10-24 15:32:09 +0800

    Signin Done

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -52,6 +52,7 @@ function signin() {
                   </p>
                 </div>
                 <div className="relative mb-6" data-te-input-wrapper-init>
+                  <label htmlFor="email">Email Address</label>
                   <input
                     type="email"
                     className="peer border-black border-2 
@@ -64,28 +65,15 @@ function signin() {
                     motion-reduce:transition-none dark:text-neutral-200 
                     dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                     id="email"
-                    placeholder="Email address"
                   />
-                  <label
-                    htmlFor="email"
-                    className="absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                  >
-                    Email address
-                  </label>
                 </div>
                 <div className="relative mb-6" data-te-input-wrapper-init>
+                  <label htmlFor="password">Password</label>
                   <input
                     type="password"
                     className="peer border-black border-2 block min-h-[auto] w-full rounded-lg bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                     id="password"
-                    placeholder="Password"
                   />
-                  <label
-                    htmlFor="password"
-                    className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                  >
-                    Password
-                  </label>
                 </div>
                 <div className="mb-6 flex items-center justify-between">
                   <div className="mb-[0.125rem] block min-h-[1.5rem] pl-[1.5rem]">
